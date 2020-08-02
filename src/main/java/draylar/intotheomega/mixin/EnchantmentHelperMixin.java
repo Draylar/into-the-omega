ae29373891e10ba23ad5287b487000c9d6692b73
Draylar <samuelhimes@gmail.com>
2020-08-02 18:20:42 -0500

    fix an issue where all enchantments would apply to every tool

--- src/main/java/draylar/intotheomega/mixin/EnchantmentHelperMixin.java
+++ src/main/java/draylar/intotheomega/mixin/EnchantmentHelperMixin.java
@@ -4,16 +4,16 @@ import draylar.intotheomega.IntoTheOmega;
 import draylar.intotheomega.impl.OmegaManipulator;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -23,21 +23,47 @@ import java.util.Optional;
 @Mixin(EnchantmentHelper.class)
 public abstract class EnchantmentHelperMixin {
 
-    @Shadow
-    public static int getEquipmentLevel(Enchantment enchantment, LivingEntity entity) {
-        return 0;
-    }
-
+    /**
+     * Modifies {@link EnchantmentHelper#getLevel(Enchantment, ItemStack)} to return the highest value between either
+     * the passed in enchantment, or the Omega variant (if applicable).
+     *
+     * @param enchantment  enchantment to check level of
+     * @param stack        stack to check for enchantments on
+     * @param cir          mixin callback info
+     */
     @Inject(
             method = "getLevel",
             at = @At("RETURN"),
             cancellable = true
     )
     private static void modifyLevel(Enchantment enchantment, ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if(((OmegaManipulator) enchantment).isOmega()) {
-            Enchantment original = ((OmegaManipulator) enchantment).getVanilla();
-            int ret = cir.getReturnValue();
-            cir.setReturnValue(ret + original.getMaxLevel());
+        int vanillaLevel = cir.getReturnValue();
+
+        // check if current enchantment instance is not omega
+        if(!((OmegaManipulator) enchantment).isOmega()) {
+            // get vanilla registry ID
+            Identifier existingID = Registry.ENCHANTMENT.getId(enchantment);
+
+            // verify minecraft namespace and convert to omega
+            if(existingID != null && existingID.getNamespace().equals("minecraft")) {
+                Identifier omegaID = IntoTheOmega.id(String.format("omega_%s", existingID.getPath()));
+
+                // verify omega enchantment exists
+                if(Registry.ENCHANTMENT.containsId(omegaID)) {
+                    Enchantment omegaEnchantment = Registry.ENCHANTMENT.get(omegaID);
+                    cir.setReturnValue(Math.max(vanillaLevel, EnchantmentHelper.getLevel(omegaEnchantment, stack)));
+                }
+            }
+        }
+
+        // make omega enchantments, if valid, return their level + the max level of the prev tool
+        else {
+            Enchantment vanilla = ((OmegaManipulator) enchantment).getVanilla();
+
+            // make sure the omega enchantment exists in the first place (solves issue where all tools have enchantments)
+            if(vanillaLevel > 0) {
+                cir.setReturnValue(vanillaLevel + vanilla.getMaxLevel());
+            }
         }
     }
 
@@ -59,33 +85,13 @@ public abstract class EnchantmentHelperMixin {
     private static int modifyLevel(CompoundTag tag, String key) {
         if(me_cachedEnchantment != null && me_cachedEnchantment.isPresent()) {
             Enchantment cached = me_cachedEnchantment.get();
+
+            // we can assume it is okay to add the max level to the omega enchantment level, because this is only called for enchantments on the tag
             if(((OmegaManipulator) cached).isOmega()) {
                 return tag.getInt("lvl") + ((OmegaManipulator) cached).getVanilla().getMaxLevel();
             }
         }
 
         return tag.getInt("lvl");
-    }
-
-    @Inject(
-            method = "getEquipmentLevel",
-            at = @At("HEAD"),
-            cancellable = true
-    )
-    private static void getEquipmentLevel(Enchantment enchantment, LivingEntity entity, CallbackInfoReturnable<Integer> cir) {
-        if(!((OmegaManipulator) enchantment).isOmega()) {
-            Identifier existingID = Registry.ENCHANTMENT.getId(enchantment);
-
-            // check minecraft namespace and convert to omega
-            if(existingID != null && existingID.getNamespace().equals("minecraft")) {
-                Identifier omegaID = IntoTheOmega.id(String.format("omega_%s", existingID.getPath()));
-
-                // verify omega enchantment exists
-                if(Registry.ENCHANTMENT.containsId(omegaID)) {
-                    Enchantment omegaEnchantment = Registry.ENCHANTMENT.get(omegaID);
-                    cir.setReturnValue(getEquipmentLevel(omegaEnchantment, entity));
-                }
-            }
-        }
     }
 }
