6380f992352d506ed537cac2eb9a848b940908bf
Draylar <samuelhimes@gmail.com>
2021-05-15 23:06:16 -0500

    add spears item tag

--- src/main/java/draylar/intotheomega/registry/OmegaTags.java
+++ src/main/java/draylar/intotheomega/registry/OmegaTags.java
@@ -2,16 +2,24 @@ package draylar.intotheomega.registry;
 
 import draylar.intotheomega.IntoTheOmega;
 import draylar.intotheomega.mixin.EntityTypeTagsAccessor;
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 
 public class OmegaTags {
 
     public static final Tag.Identified<EntityType<?>> END_CREATURE = register("end_creature");
     public static final Tag.Identified<EntityType<?>> SLIME_CREATURE = register("slime_creature");
+    public static final Tag<Item> SPEARS = registerItemTag("spears");
 
     private static Tag.Identified<EntityType<?>> register(String id) {
         return EntityTypeTagsAccessor.callRegister(IntoTheOmega.id(id).toString());
+    }
+
+    private static Tag<Item> registerItemTag(String id) {
+        return TagRegistry.item(IntoTheOmega.id(id));
     }
 
     public static void init() {
