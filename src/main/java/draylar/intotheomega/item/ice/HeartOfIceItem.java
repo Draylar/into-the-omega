66fd743688bafcaed285938f4e78a62b9f03015b
Draylar <samuelhimes@gmail.com>
2021-05-17 15:18:15 -0500

    add heart of ice tooltip

--- src/main/java/draylar/intotheomega/item/ice/HeartOfIceItem.java
+++ src/main/java/draylar/intotheomega/item/ice/HeartOfIceItem.java
@@ -3,6 +3,16 @@ package draylar.intotheomega.item.ice;
 import dev.emi.trinkets.api.SlotGroups;
 import dev.emi.trinkets.api.Slots;
 import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class HeartOfIceItem extends TrinketItem {
 
@@ -13,5 +23,13 @@ public class HeartOfIceItem extends TrinketItem {
     @Override
     public boolean canWearInSlot(String group, String slot) {
         return group.equals(SlotGroups.CHEST) && slot.equals(Slots.NECKLACE);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(LiteralText.EMPTY);
+        tooltip.add(new TranslatableText("intotheomega.heart_of_ice.1").formatted(Formatting.GRAY));
+        tooltip.add(new LiteralText("   ").append(new TranslatableText("intotheomega.heart_of_ice.2")).formatted(Formatting.GRAY));
     }
 }
