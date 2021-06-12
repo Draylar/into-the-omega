2d7283df24f272d5526956c85f2af7c3effd31f6
Draylar <samuelhimes@gmail.com>
2021-06-11 22:39:47 -0500

    update entity registry to be much cleaner

--- src/main/java/draylar/intotheomega/registry/OmegaEntities.java
+++ src/main/java/draylar/intotheomega/registry/OmegaEntities.java
@@ -27,51 +27,90 @@ public class OmegaEntities {
 
     public static final EntityType<ChorusCowEntity> CHORUS_COW = register(
             "chorus_cow",
-            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, ChorusCowEntity::new).dimensions(EntityDimensions.fixed(0.9F, 1.4F)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.CREATURE, ChorusCowEntity::new)
+                    .dimensions(EntityDimensions.fixed(0.9F, 1.4F))
+                    .build());
 
     public static final EntityType<InanisEntity> INANIS = register(
             "inanis",
-            FabricEntityTypeBuilder.<InanisEntity>create(SpawnGroup.MISC, InanisEntity::new).dimensions(EntityDimensions.fixed(.5f, .5f)).build());
+            FabricEntityTypeBuilder
+                    .<InanisEntity>create(SpawnGroup.MISC, InanisEntity::new)
+                    .dimensions(EntityDimensions.fixed(.5f, .5f))
+                    .build());
 
     public static final EntityType<DualInanisEntity> DUAL_INANIS = register(
             "dual_inanis",
-            FabricEntityTypeBuilder.<DualInanisEntity>create(SpawnGroup.MISC, DualInanisEntity::new).dimensions(EntityDimensions.fixed(.5f, .5f)).build());
+            FabricEntityTypeBuilder
+                    .<DualInanisEntity>create(SpawnGroup.MISC, DualInanisEntity::new)
+                    .dimensions(EntityDimensions.fixed(.5f, .5f))
+                    .build());
 
     public static final EntityType<VioletUnionBladeEntity> VIOLET_UNION_BLADE = register(
             "violet_union_blade",
-            FabricEntityTypeBuilder.<VioletUnionBladeEntity>create(SpawnGroup.MISC, VioletUnionBladeEntity::new).dimensions(EntityDimensions.fixed(.5f, .5f)).build());
+            FabricEntityTypeBuilder
+                    .<VioletUnionBladeEntity>create(SpawnGroup.MISC, VioletUnionBladeEntity::new)
+                    .dimensions(EntityDimensions.fixed(.5f, .5f))
+                    .build());
 
     public static final EntityType<VoidMatrixEntity> VOID_MATRIX = register(
             "void_matrix",
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, VoidMatrixEntity::new).forceTrackedVelocityUpdates(true).dimensions(EntityDimensions.fixed(4, 4)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.MONSTER, VoidMatrixEntity::new)
+                    .forceTrackedVelocityUpdates(true)
+                    .dimensions(EntityDimensions.fixed(4, 4))
+                    .build());
 
     public static final EntityType<EnigmaKingEntity> ENIGMA_KING = register(
             "enigma_king",
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, EnigmaKingEntity::new).dimensions(EntityDimensions.fixed(1, 2.2f)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.MONSTER, EnigmaKingEntity::new)
+                    .dimensions(EntityDimensions.fixed(1, 2.2f))
+                    .build());
 
     public static final EntityType<OmegaSlimeEmperorEntity> OMEGA_SLIME_EMPEROR = register(
             "omega_slime_emperor",
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, OmegaSlimeEmperorEntity::new).dimensions(EntityDimensions.fixed(6, 6)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.MONSTER, OmegaSlimeEmperorEntity::new)
+                    .dimensions(EntityDimensions.fixed(6, 6))
+                    .build());
 
     public static final EntityType<MatriteEntity> MATRITE = register(
             "matrite",
-            FabricEntityTypeBuilder.<MatriteEntity>create(SpawnGroup.MONSTER, MatriteEntity::new).dimensions(EntityDimensions.fixed(.5f, .5f)).build());
+            FabricEntityTypeBuilder
+                    .<MatriteEntity>create(SpawnGroup.MONSTER, MatriteEntity::new)
+                    .dimensions(EntityDimensions.fixed(.5f, .5f))
+                    .build());
 
     public static final EntityType<OmegaSlimeEntity> OMEGA_SLIME = register(
             "omega_slime",
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, OmegaSlimeEntity::new).dimensions(EntityDimensions.fixed(1f, 1f)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.MONSTER, OmegaSlimeEntity::new)
+                    .dimensions(EntityDimensions.fixed(1f, 1f))
+                    .build());
 
     public static final EntityType<MatrixBombEntity> MATRIX_BOMB = register(
             "matrix_bomb",
-            FabricEntityTypeBuilder.<MatrixBombEntity>create(SpawnGroup.MISC, MatrixBombEntity::new).dimensions(EntityDimensions.fixed(.25f, .25f)).build());
+            FabricEntityTypeBuilder
+                    .<MatrixBombEntity>create(SpawnGroup.MISC, MatrixBombEntity::new)
+                    .dimensions(EntityDimensions.fixed(.25f, .25f))
+                    .build());
 
     public static final EntityType<ObsidianThornEntity> OBSIDIAN_THORN = register(
             "obsidian_thorn",
-            FabricEntityTypeBuilder.<ObsidianThornEntity>create(SpawnGroup.MISC, ObsidianThornEntity::new).dimensions(EntityDimensions.changing(.25f, .25f)).build());
+            FabricEntityTypeBuilder
+                    .<ObsidianThornEntity>create(SpawnGroup.MISC, ObsidianThornEntity::new)
+                    .dimensions(EntityDimensions.changing(.25f, .25f))
+                    .build());
 
     public static final EntityType<OmegaSlimeMountEntity> OMEGA_SLIME_MOUNT = register(
             "omega_slime_mount",
-            FabricEntityTypeBuilder.<OmegaSlimeMountEntity>create(SpawnGroup.MISC, OmegaSlimeMountEntity::new).trackRangeBlocks(128).trackedUpdateRate(4).dimensions(EntityDimensions.fixed(.75f, .75f)).build());
+            FabricEntityTypeBuilder
+                    .<OmegaSlimeMountEntity>create(SpawnGroup.MISC, OmegaSlimeMountEntity::new)
+                    .trackRangeBlocks(128)
+                    .trackedUpdateRate(4)
+                    .dimensions(EntityDimensions.fixed(.75f, .75f))
+                    .build());
 
     public static final EntityType<FrostedEyeEntity> FROSTED_EYE = register(
             "frosted_eye",
@@ -85,15 +124,25 @@ public class OmegaEntities {
 
     public static final EntityType<AbyssalRiftEntity> ABYSSAL_RIFT = register(
             "abyssal_rift",
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, AbyssalRiftEntity::new).trackRangeBlocks(1000).dimensions(EntityDimensions.fixed(5, 5)).build());
+            FabricEntityTypeBuilder
+                    .create(SpawnGroup.MONSTER, AbyssalRiftEntity::new)
+                    .trackRangeBlocks(1000)
+                    .dimensions(EntityDimensions.fixed(5, 5))
+                    .build());
 
     public static final EntityType<VoidMatrixBeamEntity> VOID_MATRIX_BEAM = register(
             "void_matrix_beam",
-            FabricEntityTypeBuilder.<VoidMatrixBeamEntity>create(SpawnGroup.MISC, VoidMatrixBeamEntity::new).dimensions(EntityDimensions.fixed(.25f, .25f)).build());
+            FabricEntityTypeBuilder
+                    .<VoidMatrixBeamEntity>create(SpawnGroup.MISC, VoidMatrixBeamEntity::new)
+                    .dimensions(EntityDimensions.fixed(.25f, .25f))
+                    .build());
 
     public static final EntityType<VoidWalkerEntity> VOID_WALKER = register(
             "void_walker",
-            FabricEntityTypeBuilder.<VoidWalkerEntity>create(SpawnGroup.MISC, VoidWalkerEntity::new).dimensions(EntityDimensions.fixed(1f, 1.8f)).build());
+            FabricEntityTypeBuilder
+                    .<VoidWalkerEntity>create(SpawnGroup.MISC, VoidWalkerEntity::new)
+                    .dimensions(EntityDimensions.fixed(1f, 1.8f))
+                    .build());
 
     public static final EntityType<FrostedEndermanEntity> FROSTED_ENDERMAN = register(
             "frosted_enderman",
